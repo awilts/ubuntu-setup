5d9835cec8aaf3b6f5c4820e0b7e81d6b6f75dbf
Alexander Wilts <alexanderwilts@googlemail.com>
2020-04-29 17:17:05 +0200

    added java installation

--- manjaroSetup.sh
+++ manjaroSetup.sh
@@ -4,7 +4,7 @@
 sudo sed -i "s|PKGEXT='.pkg.tar.xz'|PKGEXT='.pkg.tar'|g" /etc/makepkg.conf
 
 #install base-devel
-sudo pacman -S base-devel --noconfirm
+sudo pacman -Sc base-devel --noconfirm
 
 #yay
 git clone https://aur.archlinux.org/yay.git
@@ -13,36 +13,38 @@ makepkg -si --noconfirm
 cd -
 
 #install apps
-yay -S --noconfirm mlocate htop visual-studio-code-bin screenfetch zip bat curl flameshot git zsh chromium terminator intellij-idea-ultimate-edition spotify
+yay -Sc --noconfirm mlocate bashtop visual-studio-code-bin chezmoi screenfetch zip bat curl flameshot git zsh chromium terminator intellij-idea-ultimate-edition spotify
 
 #oh-my-zsh
 yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-cp -rf configs/.zshrc ~/.zshrc
 
 #nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | zsh
 
-#sdkman 
+#sdkman + java 
 curl -s "https://get.sdkman.io" | zsh
+export SDKMAN_DIR="/home/alex/.sdkman"
+[[ -s "/home/alex/.sdkman/bin/sdkman-init.sh" ]] && source "/home/alex/.sdkman/bin/sdkman-init.sh"
+sdk i java
+echo 'export JAVA_HOME=/home/alex/.sdkman/candidates/java/current' >> ~/.profile
 
-./gnomeSetup.sh
-
-#gnome extensions
-#todo
-#https://extensions.gnome.org/extension/779/clipboard-indicator/
-#add hotkeys
-#backslide
-
-#chezmoi
+#setup dotfiles
+#chezmoi init https://github.com/awilts/dotfiles.git
     #vscode
     #intellij
     #terminator
+    #git
     #backslide ?
     #clipboard indicator ?
 
+#setup gnome
+./gnomeSetup.sh
 
-    polybar?
-
-    spotify theme von norb?
+#gnome extensions
+#https://extensions.gnome.org/extension/779/clipboard-indicator/
+#add hotkeys
+#backslide
+#polybar?
+#spotify theme von norb?
